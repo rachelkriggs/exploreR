26f78ad632d62ea06381d1565012fbdeba0ce32d
Jim Pushor <jimpushor@hotmail.com>
2019-02-10 22:16:55 -0800

    Draft done of missing_values and size.R

--- R/size.R
+++ R/size.R
@@ -1,3 +1,19 @@
-size <- function(x) {
+# Remove toy data frame before submitting
+toy_data <- data.frame("letters" = c("a", "b", NA, "d"),
+                       "numbers" = c(1, 4, 6, NA),
+                       "booleans" = c(NA, FALSE, NA, TRUE),
+                       stringsAsFactors = FALSE)
 
+
+# Missing Values Function
+size <- function(x) {
+  
+  rows<- nrow(x)
+  columns<- ncol(x)
+  size_in_memory<- as.numeric(object.size(x))
+  
+  data.frame(rows, columns, size_in_memory)
 }
+
+# Function call with toy data set
+size(toy_data)

0a359fc658833696926ff281324efc37805fab6a
Rachel K Riggs <rachelkriggs@gmail.com>
2019-02-25 21:57:27 -0800

    rewrote variable_summary function

--- R/variable_summary.R
+++ R/variable_summary.R
@@ -6,14 +6,35 @@ toy_data <- data.frame("letters" = c("a", "b", NA, "d"),
 
 # variable_summary function
 variable_summary <- function(x){
-  type <- vector("character", ncol(x))
-  number <- vector("integer", ncol(x))
-  for (i in seq_along(x)){
-    type[[i]] <- typeof(x[[i]])
-    number[[i]] <- sum(!is.na(x[i]))
+
+  # initialize counters for each variable type
+  num_count = 0
+  char_count = 0
+  bool_count = 0
+  date_count = 0
+  other_count = 0
+
+  # get the type of all variables
+  var_types <- sapply(x, class)
+
+  # count the types
+  for (i in 1:length(var_types)) {
+    if (test[[i]] == "numeric") {
+      num_count = num_count + 1
+    } else if (test[[i]] == "character") {
+      char_count = char_count + 1
+    } else if (test[[i]] == "logical") {
+      bool_count = bool_count + 1
+    } else if (test[[i]] == "Date") {
+      date_count = date_count + 1
+    } else {
+      other_count = other_count + 1
+    }
   }
-  data.frame("variable_type" = type,
-             "count" = number)
+
+  # return a data frame of the summary results
+  data.frame("variable type" = c("numeric", "character", "boolean", "date", "other"),
+             "count" = c(num_count, char_count, bool_count, date_count, other_count))
 }
 
 # function call for dev purposes only
